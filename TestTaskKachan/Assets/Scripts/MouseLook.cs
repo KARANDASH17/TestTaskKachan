ae9c509bc2bff2c131124e4e193c4c7ffcc9ffc9
KARANDASH17 <146541093+KARANDASH17@users.noreply.github.com>
2024-10-19 13:11:32 +0300

    Add Player Controller
    Прописал контроллер для игрока

--- TestTaskKachan/Assets/Scripts/MouseLook.cs
+++ TestTaskKachan/Assets/Scripts/MouseLook.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseLook : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    private float _xRotation = 0f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
